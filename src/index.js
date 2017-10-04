f270d97973db1d7f9a53e8ddb74a8e7aa25520c7
Kaz Yamashita <ymkzly@gmail.com>
2017-10-04 17:10:16 +0900

    Clean file up

--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { compose, createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import { Route } from 'react-router-dom'
+import { ConnectedRouter, routerMiddleware } from 'react-router-redux'
+import createHistory from 'history/createBrowserHistory'
+import reducers from './reducers'
+import Header from './components/Header'
+import About from './components/About'
+import Work from './components/Work'
 import './index.css'
 
-import App from './components/App'
+const history = createHistory()
+const store = compose(applyMiddleware(routerMiddleware(history)))(createStore)(reducers)
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <div>
+        <Header />
+        <Route exact path="/" component={About} />
+        <Route path="/about" component={About} />
+        <Route path="/work" component={Work} />
+      </div>
+    </ConnectedRouter>
+  </Provider>,
+  document.querySelector('#root')
+)
