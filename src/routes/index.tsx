11e668591b5b08fd789b4ebe224e88f54fd03c52
ymkz <ymkzly@gmail.com>
2018-07-10 23:37:39 +0900

    fix: redirect eject to routes dir

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,8 @@
 import loadable from 'loadable-components'
+import * as React from 'react'
+import { Redirect } from 'react-router-dom'
 
 export const About = loadable(() => import('src/components/pages/About'))
 export const Act = loadable(() => import('src/components/pages/Act'))
 export const Work = loadable(() => import('src/components/pages/Work'))
+export const NotFondRedirectToRoot = () => <Redirect to="/" />
