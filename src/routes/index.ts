a6af748def082535d07ccbdca348f99839598ab5
ymkz <ymkzly@gmail.com>
2018-11-23 03:32:33 +0900

    chore(webpack): magic comments for preload

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,20 @@
 import { lazy } from 'react'
 
 export const Home = lazy(() =>
-  import(/* webpackChunkName: "home", webpackPrefetch: true */ 'src/pages/Home')
+  import(/* webpackChunkName: "home" */
+  /* webpackPrefetch: true */
+  /* webpackPreload: true */
+  'src/pages/Home')
 )
 export const About = lazy(() =>
-  import(/* webpackChunkName: "about", webpackPrefetch: true */ 'src/pages/About')
+  import(/* webpackChunkName: "about" */
+  /* webpackPrefetch: true */
+  /* webpackPreload: true */
+  'src/pages/About')
 )
 export const Work = lazy(() =>
-  import(/* webpackChunkName: "work", webpackPrefetch: true */ 'src/pages/Work')
+  import(/* webpackChunkName: "work" */
+  /* webpackPrefetch: true */
+  /* webpackPreload: true */
+  'src/pages/Work')
 )
