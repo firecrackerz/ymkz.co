d6249d561020c6bf6a0f540c6d0d136e7af72d10
ymkz <ymkzly@gmail.com>
2018-12-11 03:33:59 +0900

    chore: rename navlink file for git

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled'
 import * as React from 'react'
-import Navlink from 'src/components/Navlink'
+import Navlink from 'src/components/Navlinks'
 import Row from 'src/components/Row'
 
 export default function Header() {
