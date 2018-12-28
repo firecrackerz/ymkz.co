7d77d50a408a69829328eb9c4f70da4b0736d54e
ymkz <ymkzly@gmail.com>
2018-12-28 15:46:49 +0900

    feat: add unit test example

---
+++ src/app/index.test.tsx
@@ -0,0 +1,12 @@
+import * as Navi from 'navi'
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import App from '.'
+import routes from '../routes'
+
+it('renders <App /> without crashing', () => {
+  const node = document.createElement('div')
+  const navigation = Navi.createMemoryNavigation({ pages: routes, url: '/' })
+  ReactDOM.render(<App navigation={navigation} />, node)
+  ReactDOM.unmountComponentAtNode(node)
+})
