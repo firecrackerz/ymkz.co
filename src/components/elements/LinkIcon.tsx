4fe67f0817b70ea467ddc2d6db4080dccc219898
ymkz <ymkzly@gmail.com>
2018-12-28 18:56:15 +0900

    chore: fixed img sizes

--- src/components/elements/LinkIcon.tsx
+++ src/components/elements/LinkIcon.tsx
@@ -11,18 +11,13 @@ interface Props {
 export default function LinkIcon({ href, src, alt }: Props) {
   return (
     <Link href={href} rel="noopener noreferrer" target="_blank">
-      <Icon src={src} alt={alt} />
+      <img src={src} alt={alt} width={36} height={36} />
     </Link>
   )
 }
 
 const Link = styled('a')`
   color: ${colors.white};
-  height: 36px;
-  width: 36px;
-`
-
-const Icon = styled('img')`
   height: 36px;
   width: 36px;
 `
