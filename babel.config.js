d7f709c242b5aaa263175cdd7a5b3ba30a025730
ymkz <ymkzly@gmail.com>
2019-02-23 18:15:08 +0900

    chore: clean-up config files

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,12 @@
 module.exports = function(api) {
   api.cache.using(() => process.env.NODE_ENV)
-  return {
-    presets: [
-      ['next/babel', { 'preset-env': { useBuiltIns: 'entry' } }],
-      ['@zeit/next-typescript/babel']
-    ],
-    plugins: [['emotion']]
-  }
+
+  const presets = [
+    ['next/babel', { 'preset-env': { useBuiltIns: 'usage' } }],
+    ['@zeit/next-typescript/babel']
+  ]
+
+  const plugins = [['emotion']]
+
+  return { presets, plugins }
 }
